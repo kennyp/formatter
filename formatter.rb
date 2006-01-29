da1d0075014883dfc7418e180156e27f54bc2194
Kenny Parnell <k.parnell@gmail.com>
2006-01-29 02:01:56 +0000

    Added temporary data.

---
+++ formatter.rb
@@ -0,0 +1,11 @@
+data = "// Fig. 1.2: fig01_02.cpp
+// A first program in C++
+#include <iostream>
+
+int main()
+{
+  std::cout << \"Welcome to C++!\\n\";
+  
+  return 0;   // indicate that program ended successfully
+}"
+
