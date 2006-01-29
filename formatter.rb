72af46edb5e959a1f62eeb5bff905579bbc6f191
Kenny Parnell <k.parnell@gmail.com>
2006-01-29 02:33:09 +0000

    Won the contest against Adam. Ruby rules.

--- formatter.rb
+++ formatter.rb
@@ -9,3 +9,38 @@ int main()
   return 0;   // indicate that program ended successfully
 }"
 
+puts "Before Comment Removal"
+puts data
+
+Comment = /\/\/.*[^\n]/
+Quote = /\"[^\"]+\"/
+
+def pull(data, holder, regex)
+  store = []
+  while data =~ regex
+    store += ["#{$&}"]
+    data.sub!(regex, holder)
+  end
+  store
+end
+
+comment = pull(data, "--::Comment::--", Comment)
+quote = pull(data, "--::Quote::--", Quote)
+
+puts
+puts "After Comment Removal"
+puts data
+
+def place(data, holder, array)
+  while array.length > 0
+    data.sub!(holder, array[0])
+    array.shift
+  end
+end
+
+place(data, "--::Comment::--", comment)
+place(data, "--::Quote::--", quote)
+
+puts
+puts "After Comment Reinsert"
+puts data
