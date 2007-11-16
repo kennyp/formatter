e78c3173cb527c08a72e37e155f540a276ae045e
Adam Wagner <awagner83@gmail.com>
2007-11-16 04:23:02 +0000

    Fixed bug in valid? method on grammar call

--- formatter/grammar.rb
+++ formatter/grammar.rb
@@ -7,26 +7,29 @@ require 'formatter/tokenized_string'
 
 # Formatter Grammar Class
 # Reads language grammar files and is used as a set of rules of tokenization
-
 class Grammar
+	
+	# Setup grammar instance
     def initialize(grammar)
         @grammar_file = grammar
         @tokenizer = {}
 		@rules = Array.new()
     end
     
+	# Ensure grammar_file is proper YAML has the necessary elements 
+	# to make a valid grammar
     def valid?
     	# ensure grammar is loaded
     	load_grammar if @tokenizer.size.zero?
 		
-		@tokenizer.each() do |token|
-			return false if token.size.zero?
+		# ensure each grammar rule is properly formed
+		@tokenizer.each_value do |token|
+			return false if token.size.zero? or not token.has_key? 'regex'
+			return false if token.has_key? 'rules' and token['rules'].size.zero?
 		end
+		
+		return true;
     end
-	
-	def has_rules?
-		@rules.length() > 0
-	end
     
     def load_grammar
     	@tokenizer = YAML.load_file( @grammar_file )
